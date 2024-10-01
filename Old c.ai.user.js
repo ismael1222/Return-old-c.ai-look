6f93cc759c9445ef353e251e144d0541e3fa350c
MaxGremory <65925132+ismael1222@users.noreply.github.com>
2024-10-01 03:00:48 -0300

    Update Old c.ai.user.js

--- Old c.ai.user.js
+++ Old c.ai.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Old c.ai
 // @namespace    https://github.com/ismael1222/Return-old-c.ai-look
-// @version      0.1.3
+// @version      0.1.4
 // @updateURL    https://return-old-c-ai-look.vercel.app/Old%20c.ai.user.js
 // @downloadURL    https://return-old-c-ai-look.vercel.app/Old%20c.ai.user.js
 // @description  Reskind the new site into good ol' looks with this script
