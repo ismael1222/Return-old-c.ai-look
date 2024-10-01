3fa13bd20162be79051da4187aeafde785eed77a
MaxGremory <65925132+ismael1222@users.noreply.github.com>
2024-10-01 03:49:06 -0300

    Update Old c.ai.user.js

--- Old c.ai.user.js
+++ Old c.ai.user.js
@@ -1,9 +1,7 @@
 // ==UserScript==
 // @name         Old c.ai
 // @namespace    https://github.com/ismael1222/Return-old-c.ai-look
-// @version      0.2.1
-// @updateURL    https://return-old-c-ai-look.vercel.app/Old%20c.ai.user.js
-// @downloadURL    https://return-old-c-ai-look.vercel.app/Old%20c.ai.user.js
+// @version      0.2.2
 // @description  Reskind the new site into good ol' looks with this script
 // @author       u/MaxGremory
 // @grant        GM_getResourceText
@@ -16,18 +14,24 @@
     'use strict';
     var lastPage = ""
     function loadMainPage(){
+        //funcion para crear nua tarjeta (solo main, se puede dejar aqui)
         function createCard(charName, imageURL, charURL , element){
             let card = `<a href="${charURL}"><div class="card"><img src="${imageURL}" alt="${charName}"><strong><p>${charName}</p></strong></div></a>`
         let container = document.querySelectorAll('.cardContainer')[element]
         container.insertAdjacentHTML('beforeend', card)
         }
+        //funcion para pegar la main page
         function pastePage(){
             let css = "<link rel='stylesheet' href='https://return-old-c-ai-look.vercel.app/mainpage.css'>"
+            //<link rel='stylesheet' href='https://return-old-c-ai-look.vercel.app/importStyles.css'>
             document.querySelector("body").insertAdjacentHTML("beforeend", css)
             let x = GM_getResourceText("htmll");
             document.querySelector('body').insertAdjacentHTML('beforeend', x);
         }
+        //cuando se sepa que la main page se cargo, se ejecuta esta
         function inicializacion(){
+
+            // Get profile picture
             let origPic = document.querySelectorAll('.object-cover.object-top')[1]
             let pic = "#"
             origPic?pic = origPic.src.split("i/80/").join("i/200/"):false;
@@ -67,7 +71,10 @@
                 document.querySelector("div.loading").classList.add("hide")
             },50)
         }
+        //se pega la main page
         pastePage();
+        //se empieza timer para ver cuando carga la main page original
+        //y cuando carga, se inicializa
         function start(){
             let testSubject = document.querySelectorAll("li.mb-6")[2].querySelectorAll(".swiper-slide.pr-2")[0]
             if(testSubject !== undefined){
@@ -82,13 +89,12 @@
     function loadChatPage(){
         console.log(1)
         let chatnumber = " 💬"+document.querySelector(".flex.gap-3 > div > div.flex").innerHTML.split(" chats").join("")
-        console.log(2)
         let nameObject = document.querySelector("p.font-semi-bold.line-clamp-1.text-ellipsis.break-anywhere.overflow-hidden.whitespace-normal")
-        console.log(3)
         let charImageINCHAT = document.querySelector(".flex.gap-3 > a")
-        console.log(4)
         nameObject.insertAdjacentHTML("beforeend", chatnumber)
         charImageINCHAT.insertAdjacentHTML("beforebegin", "<a href='/'>⮜</a>")
+        let css = "<link rel='stylesheet' href='https://return-old-c-ai-look.vercel.app/chatpage.css'>"
+        document.querySelector("body").insertAdjacentHTML("beforeend", css)
 
     }
     setInterval(()=>{
@@ -105,6 +111,8 @@
                 console.error("PAGE NOT RECOGNIZED. PLEASE REPORT THIS ISSUE ALONG WITH THE URL PROVIDED ON THIS MESSAGE.\n"+window.location.href)
                 lastPage = window.location.href
             }
+            //window.location.href == "https://character.ai/"?loadMainPage():false
+
         }
     }, 1000)
 
